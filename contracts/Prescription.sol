e71b52abe9c83e91f2a560bb0ef76fac5744c35d
Ali Ariff <ali.ariff12@gmail.com>
2017-11-12 00:49:59 +0100

    update

--- contracts/Prescription.sol
+++ contracts/Prescription.sol
@@ -1,5 +1,4 @@
 import "./Patient.sol";
-import "./Doctor.sol";
 
 pragma solidity ^0.4.8;
 
@@ -9,7 +8,7 @@ contract Prescription {
     address patient;
     bytes32 drugName;
     bool delivered;
-
+    
     function Prescription(address doctorP, address patientP, bytes32 drugNameP, bool deliveredP) public {
         Patient p = Patient(patientP);
         doctor = doctorP;
@@ -19,56 +18,34 @@ contract Prescription {
         id = p.getNoOfPrescriptions();
         p.addPrescription(this);
     }
-
+    
     function getId() public constant returns(uint) {
         return id;
     }
-
+    
     function setId(uint newId) public {
         id = newId;
     }
-
+    
     function getDoctor() public constant returns(address) {
         return doctor;
     }
-
+    
     function getPatient() public constant returns(address) {
         return patient;
     }
-
+    
     function getDrugName() public constant returns(bytes32) {
         return drugName;
     }
-
+    
     function getDelivered() public constant returns(bool) {
         return delivered;
     }
-
+    
     function deliver() public {
         delivered = true;
         Patient p = Patient(patient);
         p.removePrescription(this);
-    }
-
-    function getData() public constant returns (string) {
-        // Doctor d = Doctor(doctor);
-        return bytes32ToString(drugName);
-    }
-
-    function bytes32ToString(bytes32 x) private returns (string) {
-        bytes memory bytesString = new bytes(32);
-        uint charCount = 0;
-        for (uint j = 0; j < 32; j++) {
-            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
-            if (char != 0) {
-                bytesString[charCount] = char;
-                charCount++;
-            }
-        }
-        bytes memory bytesStringTrimmed = new bytes(charCount);
-        for (j = 0; j < charCount; j++) {
-            bytesStringTrimmed[j] = bytesString[j];
-        }
-        return string(bytesStringTrimmed);
     }
 }
