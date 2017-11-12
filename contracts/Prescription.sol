3ad1b0dbdab8ac8c71e4f3f7a907ef940c55b092
Ali Ariff <ali.ariff12@gmail.com>
2017-11-12 01:40:00 +0100

    update

--- contracts/Prescription.sol
+++ contracts/Prescription.sol
@@ -8,9 +8,9 @@ contract Prescription {
     address patient;
     bytes32 drugName;
     bool delivered;
-    
+
     function Prescription(address doctorP, address patientP, bytes32 drugNameP, bool deliveredP) public {
-        Patient p = Patient(patientP);
+        var p = Patient(patientP);
         doctor = doctorP;
         patient = patientP;
         drugName = drugNameP;
@@ -18,34 +18,34 @@ contract Prescription {
         id = p.getNoOfPrescriptions();
         p.addPrescription(this);
     }
-    
+
     function getId() public constant returns(uint) {
         return id;
     }
-    
+
     function setId(uint newId) public {
         id = newId;
     }
-    
+
     function getDoctor() public constant returns(address) {
         return doctor;
     }
-    
+
     function getPatient() public constant returns(address) {
         return patient;
     }
-    
+
     function getDrugName() public constant returns(bytes32) {
         return drugName;
     }
-    
+
     function getDelivered() public constant returns(bool) {
         return delivered;
     }
-    
+
     function deliver() public {
         delivered = true;
-        Patient p = Patient(patient);
+        var p = Patient(patient);
         p.removePrescription(this);
     }
 }
